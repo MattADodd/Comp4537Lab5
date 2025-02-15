935ede66bd56d4505e1fa4d5d01337cc2443b4a9
MattADodd <121894934+MattADodd@users.noreply.github.com>
2025-02-15 10:12:09 -0800

    test

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: DB_PORT,
+  port: 25060,
   ssl: process.env.DB_SSL === 'REQUIRED' ? { rejectUnauthorized: false } : null
 });
 
